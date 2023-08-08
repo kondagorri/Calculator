abad823dec20b362da5d9ed6084e4c929c10602d
k.ruchitha <138984449+kondagorri@users.noreply.github.com>
2023-08-08 20:56:25 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,43 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero"
+
+print("Select operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+print("5. Exit")
+while True:
+    choice = input("Enter choice (1/2/3/4/5): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print("Result:", add(num1, num2))
+        elif choice == '2':
+            print("Result:", subtract(num1, num2))
+        elif choice == '3':
+            print("Result:", multiply(num1, num2))
+        elif choice == '4':
+            print("Result:", divide(num1, num2))
+
+    else:
+        if choice =='5':
+            print("Exiting Calculator")
+            break
+        else:
+            print("Invalid Input. Please try again.")
